70188891514cd5cd87584e6d7a2fbd710781d2a0
zkim <zachary.kim@gmail.com>
2011-03-20 21:30:33 -0600

    Bump to 0.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject cljs-contrib "0.1-SNAPSHOT"
+(defproject cljs-contrib "0.2-SNAPSHOT"
   :description "Random cljs libraries"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[swank-clojure "1.2.0"]
-                     [cljs "0.1-SNAPSHOT"]
-                     [lein-cljs "0.1-SNAPSHOT"]]
+                     [cljs "0.2-SNAPSHOT"]
+                     [lein-cljs "0.2-SNAPSHOT"]]
   :cljs {:source-path "src"
          :source-output-path "jsout"
          :test-output-path "jsout"
