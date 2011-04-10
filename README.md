2d87244f6dd54b6d4f0e96702bb4f7d87439b18e
zkim <zachary.kim@gmail.com>
2011-04-10 03:55:12 -0600

    Bump to 0.2.1

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Random libraries for [cljs](http://github.com/zkim/cljs).
 
 ## Usage
 
-lein / cake: `[cljs-contrib "0.2"]`.
+lein / cake: `[cljs-contrib "0.2.1"]`.
 
 Note, this is a jar with cljs sources.
 
--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject cljs-contrib "0.2"
+(defproject cljs-contrib "0.2.1"
   :description "Random cljs libraries"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[swank-clojure "1.2.0"]
-                     [cljs "0.2"]
-                     [lein-cljs "0.2"]
+                     [cljs "0.2.1"]
+                     [lein-cljs "0.2.1"]
                      [lein-clojars "0.6.0"]]
   :cljs {:source-path "src"
          :source-output-path "jsout"
